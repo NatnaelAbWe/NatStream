742496514cd1585d978fa811d3fd76eef8dee736
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-10-20 13:52:43 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-bold text-2xl text-green-500">NatStream</h1>
+    </>
+  );
+}
+export default App;
